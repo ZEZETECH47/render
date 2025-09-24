53b59375df996a91eb755b34cf63aef92e967148
ZEZETECH47 <humphreymbise75@gmail.com>
2025-09-24 16:55:41 +0300

    Update pair.js

--- pair.js
+++ pair.js
@@ -6,7 +6,7 @@ const fs = require('fs');
 let router = express.Router()
 const pino = require("pino");
 const {
-    default: Bmb_Tech,
+    default: Faith_Tech,
     useMultiFileAuthState,
     delay,
     makeCacheableSignalKeyStore,
@@ -20,13 +20,13 @@ function removeFile(FilePath){
 router.get('/', async (req, res) => {
     const id = makeid();
     let num = req.query.number;
-        async function BMB_TECH_PAIR_CODE() {
+        async function FAITH_TECH_PAIR_CODE() {
         const {
             state,
             saveCreds
         } = await useMultiFileAuthState('./temp/'+id)
      try {
-            let Pair_Code_By_Bmb_Tech = Bmb_Tech({
+            let Pair_Code_By_Faith_Tech = Bmb_Tech({
                 auth: {
                     creds: state.creds,
                     keys: makeCacheableSignalKeyStore(state.keys, pino({level: "fatal"}).child({level: "fatal"})),
@@ -38,13 +38,13 @@ router.get('/', async (req, res) => {
              if(!Pair_Code_By_Bmb_Tech.authState.creds.registered) {
                 await delay(1500);
                         num = num.replace(/[^0-9]/g,'');
-                            const code = await Pair_Code_By_Bmb_Tech.requestPairingCode(num)
+                            const code = await Pair_Code_By_Faith_Tech.requestPairingCode(num)
                  if(!res.headersSent){
                  await res.send({code});
                      }
                  }
-            Pair_Code_By_Bmb_Tech.ev.on('creds.update', saveCreds)
-            Pair_Code_By_Bmb_Tech.ev.on("connection.update", async (s) => {
+            Pair_Code_By_faith_Tech.ev.on('creds.update', saveCreds)
+            Pair_Code_By_faith_Tech.ev.on("connection.update", async (s) => {
                 const {
                     connection,
                     lastDisconnect
@@ -56,7 +56,7 @@ router.get('/', async (req, res) => {
                let b64data = Buffer.from(data).toString('base64');
                let session = await Pair_Code_By_Bmb_Tech.sendMessage(Pair_Code_By_Bmb_Tech.user.id, { text: ' '+ b64data });
 
-               let BMB_TECH_TEXT = `
+               let FAITH_TECH_TEXT = `
 
 
 
@@ -66,22 +66,22 @@ router.get('/', async (req, res) => {
 *💪 Empowering Your Experience with bmb tech Bot*
 
 *👥 Join to group free chat enjoy with friends🤙*
-🖇️ https://chat.whatsapp.com/BKoqNbYGCkK5apBNP0nzI3
+🖇️ 
 
 *🌟 Show your support by giving our repo a star! 🌟*
-🔗 https://github.com/Dev-bmbtech/BMB-XMD
+🔗
 
 *💭 Need help? Join our support channel:*
 📢 💬
-https://whatsapp.com/channel/0029VawO6hgF6sn7k3SuVU3z
+
 
 *📚 Learn & Explore More with Tutorials:*
-🪄 YouTube Channel https://www.youtube.com/@bmb-xmd
+🪄 YouTube Channel
 
 *🥀 Powered by dev bmb Bot & bmb tech Inc 🥀*
 *Together, we build the future of automation! 🚀*
 `
- await Pair_Code_By_Bmb_Tech.sendMessage(Pair_Code_By_Bmb_Tech.user.id,{text:BMB_TECH_TEXT},{quoted:session})
+ await Pair_Code_By_Bmb_Tech.sendMessage(Pair_Code_By_faith_Tech.user.id,{text:FAITH_TECH_TEXT},{quoted:session})
  
 
         await delay(100);
@@ -89,7 +89,7 @@ https://whatsapp.com/channel/0029VawO6hgF6sn7k3SuVU3z
         return await removeFile('./temp/'+id);
             } else if (connection === "close" && lastDisconnect && lastDisconnect.error && lastDisconnect.error.output.statusCode != 401) {
                     await delay(10000);
-                    BMB_TECH_PAIR_CODE();
+                    FAITH_TECH_PAIR_CODE();
                 }
             });
         } catch (err) {
@@ -100,6 +100,6 @@ https://whatsapp.com/channel/0029VawO6hgF6sn7k3SuVU3z
          }
         }
     }
-    return await BMB_TECH_PAIR_CODE()
+    return await FAITH_TECH_PAIR_CODE()
 });
 module.exports = router
